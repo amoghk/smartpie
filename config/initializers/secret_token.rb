6734524f774199eaac2c03e506e0098de0b8a2a4
amogh.kulkarni <amogh.kulkarni@flipkart.com>
2013-10-26 01:00:50 +0530

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Smartpie::Application.config.secret_token = '373c7533ac63cdc2fa2ac9a64aa649316c3dbd0df2904d1f412ff8a4d62ea3866cbb79b52e14523b30ee5cdb300e75358f20aef79292bd7cf61a2769b1d70aa3'
